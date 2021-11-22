46a0040b8c601385b50f32e6e0bc7627600c51dd
xavicolmenero <67141230+xavicolmenero@users.noreply.github.com>
2021-11-22 23:06:59 +0100

    Update guestbook.js

--- guestbook.js
+++ guestbook.js
@@ -1,7 +1,7 @@
 /**
  * Web application
  */
-const apiUrl = '';
+const apiUrl = 'https://765dbfbc.eu-gb.apigw.appdomain.cloud/guestbook';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
